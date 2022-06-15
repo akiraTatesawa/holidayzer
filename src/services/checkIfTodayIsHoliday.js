80e6da9560c053c4cbbb396b0cfae2614c7d34c7
Arthur Akira <arthurak10.at@gmail.com>
2022-06-15 20:24:41 -0300

    fix: organize boolean variable

--- src/services/checkIfTodayIsHoliday.js
+++ src/services/checkIfTodayIsHoliday.js
@@ -3,8 +3,11 @@ import { holidays } from "../holidays.js";
 export function checkIfTodayIsHoliday() {
   const todayDate = new Date();
   const todayString = todayDate.toLocaleDateString("en-US");
+  const isTodayHoliday = holidays.some(
+    (holiday) => holiday.date === todayString
+  );
 
-  if (holidays.some((holiday) => holiday.date === todayString)) {
+  if (isTodayHoliday) {
     const holiday = holidays.find((el) => el.date === todayString);
 
     return `Sim, hoje é ${holiday.name}`;
